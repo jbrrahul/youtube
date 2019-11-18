b22dcf1a3b5574a6df183f4b9f4f49f9f050b9c0
rahul <rahul.gahlot@sunarctechnoogies.com>
2019-11-18 23:43:15 +0530

    design and layout chnages

--- README.md
+++ README.md
@@ -1,15 +1,45 @@
-# Youtube Broadcasting
+# YouTube Video Broadcasting
 
-A youtube stream video viewer with chatting.
+YouTube Video Broadcasting is a framework to list all videos of a Chanel. Users can do live chats for the live streaming videos.
 
-* [Getting started](#getting-started)
+## Technologies 
+jQuery
 
-## Getting started
-Install the files using git repsitiory or download 
-Open index.html file in browser
-Authancate using youtube account 
-Youtube stream video list appear
-Click on live strea video 
-Video will strat to display with the active chat session
-Type message and click on send to send the message
-Click to user name to see chat user's full details
+JavaScript
+
+CSS3
+
+HTML5
+
+YouTube API V3
+
+
+## Installation
+
+Clone repository or download zip file and extract to a location.
+
+Open yt.js file from src folder
+
+Add google API key and client ID to integrate with google account
+
+Execute index.php file in the browser.
+
+
+
+## Usage
+Click Get broadcast videos button and authenticate with google account
+
+After successful login select your desired youTube Chanel.
+
+A list of all uploaded videos will display on the page.
+
+Click Any video to open full details 
+
+Click on live streaming video and use the chatting functionality.
+
+## Note
+Google is not providing API access to localhost or any IP so to see the proper functionality deplay code on a server with a domain name.
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/js/yt.js
+++ src/js/yt.js
@@ -1,17 +1,68 @@
  function authenticate() {
     return gapi.auth2.getAuthInstance()
-        .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
+        .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
         .then(function() { console.log("Sign-in successful"); },
               function(err) { console.error("Error signing in", err); });
   }
-  function loadClient() {
-    gapi.client.setApiKey("AIzaSyCShoduqS-n2khomesmAwIyv2mHhTE0NHw");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); });
+   function executemsg(cid) {
+    return gapi.client.youtube.liveChatMessages.list({
+      "liveChatId": cid,
+      "part": "id,snippet,authorDetails"
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              },
+              function(err) { console.error("Execute error", err); });
   }
-  // Make sure the client is loaded and sign-in is complete before calling this method.
-  function execute() {
+  function getData(vid,cid,title)
+  {
+	 
+  }
+ 
+  jQuery(document).ready(function($){
+	   setInterval(function(){ fatchmsg()}, 1000);
+  $(document).on('click', '.videos', function() {
+    var title = $(this).attr('title');
+    var cid = $(this).attr('cid');
+    var vid = $(this).attr('vid');
+    $('.cid').val(cid);
+
+	 $('.newsletter-inner.section-inner').css('opacity',1);
+	 $('.newsletter-header.text-center.is-revealing h2').text(title);
+	  $('.newsletter-header.text-center.is-revealing .iframevd').attr('src','https://www.youtube.com/embed/'+vid);
+	 // executemsg(cid);
+	fatchmsg();
+ 
+  });});
+  function fatchmsg()
+  {
+	  var cid =  $('.cid').val();
+if(cid){
+	   return gapi.client.youtube.liveChatMessages.list({
+      "liveChatId": cid,
+      "part": "id,snippet,authorDetails"
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+			var	msgData = response.result.items;
+				
+				 $('.msgData ul').html('<li>-</li>');
+				  $.each(msgData, function( l,v ) {
+					  var msgs = v.snippet.displayMessage;
+					  var authorDetails = v.authorDetails.displayName;
+					  var channelUrl = v.authorDetails.channelUrl;
+					  $('.msgData ul').append('<li class="msg"><a target="_blank" href="'+channelUrl+'">'+authorDetails+':</a> <p>'+msgs+'</p></li>');
+				  });
+				$('.msgData ul').animate({ scrollTop: 9999 }, 'slow');
+				
+              },
+              function(err) { console.error("Execute error", err); });
+} }
+ 
+  
+    function execute() {
     return gapi.client.youtube.liveBroadcasts.list({
       "part": "id,snippet,contentDetails",
       "broadcastType": "event",
@@ -20,9 +71,65 @@
         .then(function(response) {
                 // Handle the results here (response.result has the parsed body).
                 console.log("Response", response);
+				//var datajs = json.parse(response);
+			var	datajs = response ;
+				//rendervideos(datajs.result.items)
+				var videos = datajs.result.items;
+				  $.each(videos, function( l,v ) {
+					  if(v.snippet.thumbnails.medium.url)
+					  {
+						  var liveChatId = v.snippet.liveChatId?v.snippet.liveChatId:0;
+ $('.hero-browser-inner.is-revealing').append('<div class="videos" vid="'+v.id+'"  cid="'+liveChatId+'"  title="'+v.snippet.title+'"><img src="'+v.snippet.thumbnails.medium.url+'"><div class="video-title"><a target="_blank" href="https://www.youtube.com/watch?v='+v.id+'">'+(v.snippet.title)+'</a></div>')
+					  }
+});
+				
               },
               function(err) { console.error("Execute error", err); });
   }
+  $(document).on('click','.sendMsg',function(){
+	  
+	  var msg = $('.txtMsg').val();
+	  $('.txtMsg').val('');
+	  var cid = $('.cid').val();
+	  executeSent(cid,msg);
+  })
+    function executeSent(cid,msg) {
+    return gapi.client.youtube.liveChatMessages.insert({
+      "part": "snippet",
+      "resource": {
+        "snippet": {
+          "liveChatId": cid,
+          "type": "textMessageEvent",
+          "textMessageDetails": {
+            "messageText": msg
+          }
+        }
+      }
+    })
+        .then(function(response) {
+			fatchmsg();
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              },
+              function(err) { console.error("Execute error", err); });
+  }
+  
+  function rendervideos(videos)
+  {
+	  $.each(videos, function( v ) {
+ $('.hero-browser-inner.is-revealing').append('<div class="videos"><img src="'+v.snippet.thumbnails.medium.url+'"><a target="_blank" href="https://www.youtube.com/watch?v='+v.id+'"><h1>'+(v.snippet.title)+'</h1></a></div>')
+});
+  	
+  }
+  function loadClient() {
+    gapi.client.setApiKey("AIzaSyCShoduqS-n2khomesmAwIyv2mHhTE0NHw");
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { execute(); console.log("GAPI client loaded for API1231231");execute(); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+			
+  }
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+
   gapi.load("client:auth2", function() {
     gapi.auth2.init({client_id: "1019178509762-lvrv63kou937okpvtl00vadct2rc998i.apps.googleusercontent.com"});
   });
